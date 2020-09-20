e86aa71e566a1941c40bdebacbc45366c3be5944
vutanguofa <vu.q.tang3@gmail.com>
2020-09-20 00:26:40 -0700

    disabled credit card FI

--- assets/js/script.js
+++ assets/js/script.js
@@ -40,7 +40,6 @@ function validateAddress() {
             return response.json();
         })
         .then(function (data) {
-            console.log("Details from address validator API: " + data);
             console.log("Shipping address is: " + data.status);
             if (data.status === "VALID") {
                 $("#validationStatus").attr("style", "background-color: #58ce7b").text("Valid address.");
