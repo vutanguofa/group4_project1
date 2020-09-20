dfb992016fd57f114cf38be1b1519a7e5678f0b3
vutanguofa <68674610+vutanguofa@users.noreply.github.com>
2020-09-19 18:38:38 -0700

    updating directory; PayPal and form changes

---
+++ assets/js/script.js
@@ -0,0 +1,156 @@
+// Start of address validation function
+function validateAddress() {
+    // addres-validator.net: https://www.address-validator.net/api.html
+    //var apiKey = "av-219b5d3a55b7bab918373de09c364935";
+    var apiKey = "";
+
+    // Declaing variables
+    var customerName = document.querySelector('#customerName').value;
+    var streetAddress = document.querySelector('#streetAddress').value;
+    var city = document.querySelector('#city').value;
+    var state = document.querySelector('#state').value;
+    var postalCode = document.querySelector('#postalCode').value;
+    var countryCode = document.querySelector('#countryCode').value;
+    // Creating a variable to store the order amount
+    var orderAmt = parseFloat(document.querySelector('#orderAmt').value);
+    var shipAmt = parseFloat(5.00);
+    var orderTotal = orderAmt + shipAmt;
+    var maskColor = document.querySelector('#maskColor').value;
+
+    // Storing variable to local storage
+    localStorage.setItem("customerName", customerName);
+    localStorage.setItem("streetAddress", streetAddress);
+    localStorage.setItem("city", city);
+    localStorage.setItem("state", state);
+    localStorage.setItem("postalCode", postalCode);
+    localStorage.setItem("countryCode", countryCode);
+    localStorage.setItem("orderAmt", orderAmt);
+    localStorage.setItem("shipAmt", shipAmt);
+    localStorage.setItem("orderTotal", orderTotal);
+    localStorage.setItem("maskColor", maskColor);
+    // End of local storage
+
+    // Shipping address API
+    fetch('https://cors-anywhere.herokuapp.com/https://api.address-validator.net/api/verify?StreetAddress=' + streetAddress +
+        '&City=' + city +
+        '&State=' + state +
+        '&PostalCode=' + postalCode +
+        '&CountryCode=' + countryCode +
+        '&APIKey=' + apiKey)
+        .then(function (response) {
+            return response.json();
+        })
+        .then(function (data) {
+            console.dir(data);
+            console.log("Shipping address is: " + data.status);
+            if (data.status === "VALID") {
+                $("#validationStatus").attr("style", "background-color: #58ce7b").text("Valid address.");
+                //$("#paypal-button-container").show();
+            } else {
+                $("#validationStatus").attr("style", "background-color: #fc665e").text("Invalid address. Please correct your shipping address.");
+                $("#paypal-button-container").show();
+            };
+        })
+        .catch((error) => {
+            console.error('Error:', error);
+        });
+};
+// End of address validation function
+
+// Hide PayPal button until address validation is successful"
+$("#paypal-button-container").hide();
+
+// Start of PayPal create order API
+paypal.Buttons({
+    style: {
+        color: 'blue',
+        shape: 'pill',
+        label: 'pay',
+        height: 40
+    },
+    createOrder: function (data, actions) {
+        return actions.order.create({
+            application_context: {
+                brand_name: 'Group Four',
+            },
+            purchase_units: [
+                {
+                    amount: {
+                        currency_code: 'USD',
+                        value: localStorage.getItem("orderTotal"),
+                        breakdown: {
+                            item_total: {
+                                currency_code: 'USD',
+                                value: localStorage.getItem("orderAmt")
+                            },
+                            shipping: {
+                                currency_code: 'USD',
+                                value: localStorage.getItem("shipAmt")
+                            }
+                        }
+                    },
+                    items: [
+                        {
+                            name: 'Face Masks',
+                            description: localStorage.getItem("maskColor"),
+                            sku: 'sku01',
+                            unit_amount: {
+                                currency_code: 'USD',
+                                value: localStorage.getItem("orderAmt")
+                            },
+                            quantity: '1',
+                            category: 'PHYSICAL_GOODS'
+                        },
+                    ],
+                    shipping: {
+                        name: {
+                            full_name: localStorage.getItem("customerName"),
+                        },
+                        address: {
+                            address_line_1: localStorage.getItem("streetAddress"),
+                            //address_line_2: data.addressline2,
+                            admin_area_2: localStorage.getItem("city"),
+                            admin_area_1: localStorage.getItem("state"),
+                            postal_code: localStorage.getItem("postalCode"),
+                            country_code: "US"
+                        }
+                    }
+                }
+            ]
+        });
+    },
+    onApprove: function (data, actions) {
+        return actions.order.capture().then(function (details) {
+            console.log('The payment was completed!');
+            console.log("data of the transaction was ", data);
+            console.log("actions of the transaction was ", actions);
+            console.log("response of the transaction was ", details);
+            alert('Thank you, ' + details.payer.name.given_name + ' ' + details.payer.name.surname + '!');
+            alert('Email address: ' + details.payer.email_address);
+            alert('Address: ' + details.payer.address.address_line_1 + '\n' + details.payer.address.address_line_2 + '\n' + details.payer.address.admin_area_2 + '\n' + details.payer.address.admin_area_1 + '\n' + details.payer.address.postal_code + '\n' + details.payer.address.country_code + '\n' + details.payer.phone.phone_number.national_number + '\n');
+        });
+    },
+    onCancel: function (data, actions) {
+        console.log('user cancelled-', data);
+    },
+    onError: function (data, actions) {
+        console.log('error occured-s', data);
+        var error = data;
+        alert(error);
+    }
+}).render('#paypal-button-container');
+//End of PayPal button
+
+// Get local storage details for shipping address
+localStorage.getItem("customerName");
+console.log("This is: " + customerName);
+localStorage.getItem("streetAddress");
+console.log(streetAddress);
+localStorage.getItem("city");
+console.log(city);
+localStorage.getItem("state");
+console.log(state);
+localStorage.getItem("postalCode");
+console.log(postalCode);
+localStorage.getItem("countryCode");
+console.log(countryCode);
