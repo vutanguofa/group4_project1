45ff3a6103180389ecec03639e135a6675788b9a
vutanguofa <vu.q.tang3@gmail.com>
2020-09-20 02:02:38 -0700

    added local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,15 +1,17 @@
+checkCustomerName();
+
 // Start of address validation function
 function validateAddress() {
-    var apiKey = "av-219b5d3a55b7bab918373de09c364935";
-    //var apiKey = "";
+    //var apiKey = "av-219b5d3a55b7bab918373de09c364935";
+    var apiKey = "";
 
     // Declaing variables
-    var customerName = document.querySelector('#customerName').value;
-    var streetAddress = document.querySelector('#streetAddress').value;
-    var city = document.querySelector('#city').value;
-    var state = document.querySelector('#state').value;
-    var postalCode = document.querySelector('#postalCode').value;
-    var countryCode = document.querySelector('#countryCode').value;
+    var customerNameField = document.querySelector('#customerName').value;
+    var streetAddressField = document.querySelector('#streetAddress').value;
+    var cityField = document.querySelector('#city').value;
+    var stateField = document.querySelector('#state').value;
+    var postalCodeField = document.querySelector('#postalCode').value;
+    var countryCodeField = document.querySelector('#countryCode').value;
     // Creating a variable to store the order amount
     var orderAmt = parseFloat(document.querySelector('#orderAmt').value);
     var shipAmt = parseFloat(5.00);
@@ -17,24 +19,24 @@ function validateAddress() {
     var maskColor = document.querySelector('#maskColor').value;
 
     // Storing variable to local storage
-    localStorage.setItem("customerName", customerName);
-    localStorage.setItem("streetAddress", streetAddress);
-    localStorage.setItem("city", city);
-    localStorage.setItem("state", state);
-    localStorage.setItem("postalCode", postalCode);
-    localStorage.setItem("countryCode", countryCode);
-    localStorage.setItem("orderAmt", orderAmt);
-    localStorage.setItem("shipAmt", shipAmt);
-    localStorage.setItem("orderTotal", orderTotal);
-    localStorage.setItem("maskColor", maskColor);
+    localStorage.setItem("customerName1", customerNameField);
+    localStorage.setItem("streetAddress1", streetAddressField);
+    localStorage.setItem("city1", cityField);
+    localStorage.setItem("state1", stateField);
+    localStorage.setItem("postalCode1", postalCodeField);
+    localStorage.setItem("countryCode1", countryCodeField);
+    localStorage.setItem("orderAmt1", orderAmt);
+    localStorage.setItem("shipAmt1", shipAmt);
+    localStorage.setItem("orderTotal1", orderTotal);
+    localStorage.setItem("maskColor1", maskColor);
     // End of local storage
 
     // Shipping address API
     fetch('https://cors-anywhere.herokuapp.com/https://api.address-validator.net/api/verify?StreetAddress=' + streetAddress +
-        '&City=' + city +
-        '&State=' + state +
-        '&PostalCode=' + postalCode +
-        '&CountryCode=' + countryCode +
+        '&City=' + cityField +
+        '&State=' + stateField +
+        '&PostalCode=' + postalCodeField +
+        '&CountryCode=' + countryCodeField +
         '&APIKey=' + apiKey)
         .then(function (response) {
             return response.json();
@@ -43,10 +45,10 @@ function validateAddress() {
             console.log("Shipping address is: " + data.status);
             if (data.status === "VALID") {
                 $("#validationStatus").attr("style", "background-color: #58ce7b").text("Valid address.");
-                $("#paypal-button-container").show();
+                //$("#paypal-button-container").show();
             } else {
                 $("#validationStatus").attr("style", "background-color: #fc665e").text("Invalid address. Please correct your shipping address.");
-                //$("#paypal-button-container").show();
+                $("#paypal-button-container").show();
             };
         })
         .catch((error) => {
@@ -75,26 +77,25 @@ paypal.Buttons({
                 {
                     amount: {
                         currency_code: 'USD',
-                        value: localStorage.getItem("orderTotal"),
+                        value: localStorage.getItem("orderTotal1"),
                         breakdown: {
                             item_total: {
                                 currency_code: 'USD',
-                                value: localStorage.getItem("orderAmt")
+                                value: localStorage.getItem("orderAmt1")
                             },
                             shipping: {
                                 currency_code: 'USD',
-                                value: localStorage.getItem("shipAmt")
+                                value: localStorage.getItem("shipAmt1")
                             }
                         }
                     },
                     items: [
                         {
                             name: 'Face Masks',
-                            description: localStorage.getItem("maskColor"),
-                            sku: 'sku01',
+                            description: localStorage.getItem("maskColor1"),
                             unit_amount: {
                                 currency_code: 'USD',
-                                value: localStorage.getItem("orderAmt")
+                                value: localStorage.getItem("orderAmt1")
                             },
                             quantity: '1',
                             category: 'PHYSICAL_GOODS'
@@ -102,14 +103,14 @@ paypal.Buttons({
                     ],
                     shipping: {
                         name: {
-                            full_name: localStorage.getItem("customerName"),
+                            full_name: localStorage.getItem("customerName1"),
                         },
                         address: {
-                            address_line_1: localStorage.getItem("streetAddress"),
+                            address_line_1: localStorage.getItem("streetAddress1"),
                             //address_line_2: data.addressline2,
-                            admin_area_2: localStorage.getItem("city"),
-                            admin_area_1: localStorage.getItem("state"),
-                            postal_code: localStorage.getItem("postalCode"),
+                            admin_area_2: localStorage.getItem("city1"),
+                            admin_area_1: localStorage.getItem("state1"),
+                            postal_code: localStorage.getItem("postalCode1"),
                             country_code: "US"
                         }
                     }
@@ -121,7 +122,6 @@ paypal.Buttons({
         return actions.order.capture().then(function (details) {
             console.log("Details from PayPal API: ", details);
             $("#productSection").hide();
-            //$("#checkoutMenu").hide();
             $("#confirmation").text("Thank you for your order! Your package will arrive within 3 to 5 business days.");
             $("#paypal-button-container").hide();
         });
@@ -137,16 +137,23 @@ paypal.Buttons({
 }).render('#paypal-button-container');
 //End of PayPal button
 
-// Get local storage details for shipping address
-// localStorage.getItem("customerName");
-// console.log("This is: " + customerName);
-// localStorage.getItem("streetAddress");
-// console.log(streetAddress);
-// localStorage.getItem("city");
-// console.log(city);
-// localStorage.getItem("state");
-// console.log(state);
-// localStorage.getItem("postalCode");
-// console.log(postalCode);
-// localStorage.getItem("countryCode");
-// console.log(countryCode);
+// // CustomerName Check
+function checkCustomerName() {
+    var checkName = localStorage.getItem("customerName1");
+    var checkStreet = localStorage.getItem("streetAddress1");
+    var checkCity = localStorage.getItem("city1");
+    var checkState = localStorage.getItem("state1");
+    var checkPostalCode = localStorage.getItem("postalCode1");
+    var checkCountryCode = localStorage.getItem("countryCode1");
+    if (checkName == null) {
+        console.log("");
+    } else {
+        // Get local storage details for shipping address
+        $("#customerName").val(checkName);
+        $("#streetAddress").val(checkStreet);
+        $("#city").val(checkCity);
+        $("#state").val(checkState);
+        $("#postalCode").val(checkPostalCode);
+        $("#countryCode").val(checkCountryCode);
+    }
+};
